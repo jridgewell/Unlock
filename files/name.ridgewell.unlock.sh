c5f85c057c34099e089c3179e2be22753a116048
Justin Ridgewell <justin@ridgewell.name>
2011-07-24 15:52:22 -0400

    Update unlock.sh to new filename, correct uninstall.

---
+++ files/name.ridgewell.unlock.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Unlock volumes at boot
+echo "Checking if volume is locked."
+if [ "`diskutil cs list | grep Locked`" ]
+	then
+		PASSWORD=security 2>&1 >/dev/null find -gs name.ridgewell.unlock "/Library/Keychains/System.keychain" \
+			| ruby -e 'print $1 if STDIN.gets =~ /^password: "(.*)"$/';
+		echo "Unlocking volume."
+		diskutil cs unlockVolume UUID -passphrase $PASSWORD
+fi
