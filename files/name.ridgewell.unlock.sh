c83d11de69b3af11dff165d72b7ae5fc1f991cef
Justin Ridgewell <justin@ridgewell.name>
2011-07-24 16:33:51 -0700

    Bug fix.

--- files/name.ridgewell.unlock.sh
+++ files/name.ridgewell.unlock.sh
@@ -5,5 +5,5 @@ if [ "`diskutil cs list | grep Locked`" ]
 	then
 		PASSWORD=`security 2>&1 >/dev/null find -gs name.ridgewell.unlock "/Library/Keychains/System.keychain" | cut -d '"' -f 2`
 		echo "Unlocking volume."
-		diskutil cs unlockVolume 32585426-99F0-4991-965C-DB5C809400B5 -passphrase $PASSWORD
+		diskutil cs unlockVolume UUID -passphrase "$PASSWORD"
 fi
