fd765c7751713fcb5e2621fcfddd8013fa49c060
Justin Ridgewell <justin@ridgewell.name>
2011-07-24 15:49:23 -0400

    Make sure uninstall uses correct delete command.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Uninstalling..."
-sudo security delete -D "Encrypted Volume Password" -l "Unlock" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"
+sudo security delete-generic-password -D "Encrypted Volume Password" -l "Unlock" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"
 sudo rm /Library/LaunchDaemons/name.ridgewell.unlock.plist
 sudo rm /Library/LaunchDaemons/unlock.sh
 
