70c0bdbbf5d2c0ac95ab1ee1b40ffe3f5e7054f1
Justin Ridgewell <justin@ridgewell.name>
2011-07-24 15:56:10 -0400

    Final revisions to uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Uninstalling..."
-sudo security >/dev/null delete-generic-password -D "Encrypted Volume Password" -l "Unlock" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"
+sudo security >/dev/null 2>&1 delete-generic-password -D "Encrypted Volume Password" -l "Unlock" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"
 sudo rm /Library/LaunchDaemons/name.ridgewell.unlock.plist
-sudo rm /Library/LaunchDaemons/unlock.sh
+sudo rm /Library/LaunchDaemons/name.ridgewell.unlock.sh
 
 echo ""
 echo "--------------------------"
