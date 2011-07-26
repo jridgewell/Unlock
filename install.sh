e4d91e1e50826acc6052469f65eca886bba504a4
Justin Ridgewell <justin@ridgewell.name>
2011-07-25 21:55:45 -0400

    Fix whitespace and output.

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ if [ "`whoami`" != "root" ]
 		echo "Attempting to re-run as root..."
 		curl https://raw.github.com/jridgewell/Unlock/dynamic/install.sh -o install.sh
 		chmod +x install.sh
+		echo "--------------------------"
 		echo ""
+
 		sudo bash ./install.sh
 		rm install.sh
 		exit
 fi
 
-vname() { echo `diskutil cs info $1 | grep "Volume Name" | cut -d : -f 2 | sed -e 's/^\ *//'` ;}
+vname() { echo `diskutil cs info $1 | grep "Volume Name" | cut -d : -f 2 | sed -e 's/^\ *//'`; }
 unlock() {
-	echo "What is the passphrase used to encrypt ${2}"
+	echo "What is the passphrase used to encrypt ${2}?"
 	read password < /dev/tty
 	# Add the password to the System keychain
 	security add -a "${1}" -D "Encrypted Volume Password" -l "Unlock: ${2}" -s "name.ridgewell.unlock" \
@@ -23,68 +25,71 @@ unlock() {
 ask() {
 	# Get the name of the volume with UUID
 	name=`vname $1`
-    echo "Do you want to unlock ${name} at boot? (y/N)"
+	echo "Do you want to unlock ${name} at boot? (y/N)"
 	read yn < /dev/tty
 	# Make user input lowercase
-    answer=`echo ${yn}| awk '{print tolower($0)}'`
-    if [[ $answer == "y" || $answer == "yes" ]]
-        then
-            unlock $1 $name
-    fi
-	echo ""
+	answer=`echo ${yn}| awk '{print tolower($0)}'`
+	if [[ $answer = "y" || $answer = "yes" ]]
+		then
+			unlock $1 $name
+	fi
 }
 
-bool=false
 mkdir tmp_install_unlock
 cd tmp_install_unlock
+bool=false
 
 # http://stackoverflow.com/questions/893585/how-to-parse-xml-in-bash#answer-2608159
 rdom() { local IFS=\> ; read -d \< E C ;}
 CSVs=`diskutil cs list -plist`
 echo $CSVs | while rdom; do
+	if [[ $E = "string" ]]
 	# All the important stuff is inside the "string" elements
-    if [[ $E = string ]]; then
-        echo "$C"
-    fi
+		then
+			echo "$C"
+	fi
 done | \
 while read LINE; do
 # Loop through all found LVGs, LVFs, LVs
-    if $bool
-    	# If this is a LV's UUID, ask if they want to unlock it
-        then
-            ask $LINE
-    fi
-    if [[ $LINE == "LV" ]]
-    	# If true, the next line will be the LV's UUID
-        then
-            bool=true
-        else
-            bool=false
-    fi
+	if $bool
+	# If this is a LV's UUID, ask if they want to unlock it
+		then
+			ask $LINE
+	fi
+	if [[ $LINE = "LV" ]]
+	# If true, the next line will be a LV's UUID
+		then
+			bool=true
+		else
+			bool=false
+	fi
 done
+
 if [ -e "UUIDs.txt" ]
-    then
-        echo "--------------------------"
-        echo "Installing..."
-        curl "https://raw.github.com/jridgewell/Unlock/master/files/name.ridgewell.unlock.plist" -o name.ridgewell.unlock.plist
-        curl "https://raw.github.com/jridgewell/Unlock/dynamic/files/name.ridgewell.unlock-start.sh" >> name.ridgewell.unlock.sh
-        while read LINE; do
-            echo "          diskutil cs unlockVolume UUID -passphrase \"\`security 2>&1 >/dev/null find -ga UUID \"/Library/Keychains/System.keychain\" | cut -d '\"' -f 2\`\" 2>/dev/null" | sed "s/UUID/$LINE/g" >> name.ridgewell.unlock.sh
-        done < "UUIDs.txt"
-        curl "https://raw.github.com/jridgewell/Unlock/dynamic/files/name.ridgewell.unlock-end.sh" >> name.ridgewell.unlock.sh
-        rm "UUIDs.txt"
-        
-        mv ./* /Library/LaunchDaemons/
-        chown root:wheel /Library/LaunchDaemons/name.ridgewell.unlock.plist
-        chown root:wheel /Library/LaunchDaemons/name.ridgewell.unlock.sh
-        chmod 644 /Library/LaunchDaemons/name.ridgewell.unlock.plist
-        chmod 755 /Library/LaunchDaemons/name.ridgewell.unlock.sh
+# We need to set up the LaunchDaemon script to unlock the volumes
+	then
+		echo "--------------------------"
+		echo ""
+		echo "Installing..."
+		curl "https://raw.github.com/jridgewell/Unlock/master/files/name.ridgewell.unlock.plist" -o name.ridgewell.unlock.plist
+		curl "https://raw.github.com/jridgewell/Unlock/dynamic/files/name.ridgewell.unlock-start.sh" >> name.ridgewell.unlock.sh
+		while read LINE; do
+			echo "		diskutil cs unlockVolume UUID -passphrase \"\`security 2>&1 >/dev/null find -ga UUID \"/Library/Keychains/System.keychain\" | cut -d '\"' -f 2\`\" 2>/dev/null" | sed "s/UUID/$LINE/g" >> name.ridgewell.unlock.sh
+		done < "UUIDs.txt"
+		curl "https://raw.github.com/jridgewell/Unlock/dynamic/files/name.ridgewell.unlock-end.sh" >> name.ridgewell.unlock.sh
+		rm "UUIDs.txt"
 
-        # Cleanup
-        cd ..
-        rm -r tmp_install_unlock
+		mv ./* /Library/LaunchDaemons/
+		chown root:wheel /Library/LaunchDaemons/name.ridgewell.unlock.plist
+		chown root:wheel /Library/LaunchDaemons/name.ridgewell.unlock.sh
+		chmod 644 /Library/LaunchDaemons/name.ridgewell.unlock.plist
+		chmod 755 /Library/LaunchDaemons/name.ridgewell.unlock.sh
 
-        echo ""
-        echo "--------------------------"
-        echo "Installed!"
+		echo "--------------------------"
+		echo ""
+		echo "Installed!"
 fi
+
+# Cleanup
+cd ..
+rm -r tmp_install_unlock
--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,11 @@
 echo "Uninstalling..."
 err=`sudo security 2>&1 >/dev/null delete-generic-password -D "Encrypted Volume Password" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"`
 while [[ $err == "password has been deleted." ]]; do
-    err=`sudo security 2>&1 >/dev/null delete-generic-password -D "Encrypted Volume Password" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"`
+	err=`sudo security 2>&1 >/dev/null delete-generic-password -D "Encrypted Volume Password" -s "name.ridgewell.unlock" "/Library/Keychains/System.keychain"`
 done
 sudo rm /Library/LaunchDaemons/name.ridgewell.unlock.plist
 sudo rm /Library/LaunchDaemons/name.ridgewell.unlock.sh
 
-echo ""
 echo "--------------------------"
+echo ""
 echo "Uninstalled."
